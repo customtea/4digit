7b2d1c52c2a10fb364dc67e79acf3ee481097e96
customtea <kon.taku8@gmail.com>
2019-05-08 11:18:54 +0900

    計算4種対応

--- 4digit.rb
+++ 4digit.rb
@@ -12,20 +12,6 @@ def operant(ope, x, y)
 
 end
 
-def print_operant(ope)
-  case ope
-  when 0
-    print "+"
-  when 1
-    print "-"
-  when 2
-    print "x"
-  when 3
-    print "/"
-  end
-
-end
-
 def r_ope(ope)
   case ope
   when 0
@@ -40,28 +26,39 @@ def r_ope(ope)
 end
 
 def calc1(frac, o1, o2, o3)
-  tmpfrac1 = operant(o1, frac[0], frac[1])
-  tmpfrac2 = operant(o2, tmpfrac1, frac[2])
-  tmpfrac3 = operant(o3, tmpfrac2, frac[3])
-  return 1000 if tmpfrac3.denominator == 0
-  tmpint = tmpfrac3.to_f
-  
+  tmpfrac = operant(o3,operant(o2,operant(o1, frac[0], frac[1]), frac[2]), frac[3])
+  return 1000 if tmpfrac.denominator == 0
+  tmpint = tmpfrac.to_f
   if tmpint == 10
     print "((#{frac[0].numerator}#{r_ope(o1)}#{frac[1].numerator})#{r_ope(o2)}#{frac[2].numerator})#{r_ope(o3)}#{frac[3].numerator}\n"
-    
-    nume = Array.new(4)
-    nume = frac.map{|num| num.numerator}
-
-    #print frac[0].numerator
-    #print_operant(o1)
-    #print frac[1].numerator
-    #print_operant(o2)
-    #print frac[2].numerator
-    #print_operant(o3)
-    #print frac[3].numerator
-    #print "\n"
   end
+end
 
+def calc2(frac, o1, o2, o3)
+  tmpfrac = operant(o3,operant(o2,frac[2], operant(o1, frac[0], frac[1])), frac[3])
+  return 1000 if tmpfrac.denominator == 0
+  tmpint = tmpfrac.to_f
+  if tmpint == 10
+    print "(#{frac[2].numerator}#{r_ope(o2)}(#{frac[0].numerator}#{r_ope(o1)}#{frac[1].numerator}))#{r_ope(o3)}#{frac[3].numerator}\n"
+  end
+end
+
+def calc3(frac, o1, o2, o3)
+  tmpfrac = operant(o3, frac[3], operant(o2, frac[2], operant(o1, frac[0], frac[1])))
+  return 1000 if tmpfrac.denominator == 0
+  tmpint = tmpfrac.to_f
+  if tmpint == 10
+    print "((#{frac[0].numerator}#{r_ope(o1)}#{frac[1].numerator})#{r_ope(o2)}#{frac[2].numerator})#{r_ope(o3)}#{frac[3].numerator}\n"
+  end
+end
+
+def calc4(frac, o1, o2, o3)
+  tmpfrac = operant(o3, operant(o1, frac[0], frac[1]) ,operant(o2, frac[2], frac[3]))
+  return 1000 if tmpfrac.denominator == 0 
+  tmpint = tmpfrac.to_f
+  if tmpint == 10
+    print "((#{frac[0].numerator}#{r_ope(o1)}#{frac[1].numerator})#{r_ope(o2)}#{frac[2].numerator})#{r_ope(o3)}#{frac[3].numerator}\n"
+  end
 end
 
 def ope_parm(frac)
